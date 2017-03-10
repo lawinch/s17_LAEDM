7d176747bf67b097f6dc3308a78a593805e2eecd
Maxim Skryabin <ms@stepic.org>
2017-03-10 16:48:36 +0300

    filename

--- lab5/main.py
+++ lab5/main.py
@@ -3,10 +3,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--user', type=int, help='user id')
-parser.add_argument('-d', '--data', type=str, help='data filename')
+parser.add_argument('-f', '--filename', type=str, help='data filename')
 
 args = parser.parse_args()
-if not args.data:
+if not args.filename:
     print('Error. No data file')
     exit(1)
 
