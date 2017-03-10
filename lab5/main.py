91f54c33b94f383a27136ed6c09a44e3d31b8770
Maxim Skryabin <ms@stepic.org>
2017-03-10 12:36:21 +0300

    change argument processing

--- lab5/main.py
+++ lab5/main.py
@@ -1,13 +1,14 @@
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--user', type=int, help='user id')
-parser.add_argument('-d', '--data', type=str, help='data file')
+parser.add_argument('-d', '--data', type=str, help='data filename')
 
 args = parser.parse_args()
-if args.data:
-    # process data
-    pass
-else:
-    print('Error. There is no data file')
+if not args.data:
+    print('Error. No data file')
     exit(1)
+
+# process data here
+print(args.data)
